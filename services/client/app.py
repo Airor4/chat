f1baec78f55a1223107abd93ded9f9258712e608
= <=>
2022-10-26 21:41:51 -0400

    updated server uri on client

--- services/client/app.py
+++ services/client/app.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def hello():
-    uri = "ws://localhost:8765"
+    uri = "ws://server:8763"
     async with websockets.connect(uri) as websocket:
         name = input("What's your name? ")
 
