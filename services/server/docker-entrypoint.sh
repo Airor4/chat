399adbaa609bd30aba2805418ef97a2758ff8be0
= <=>
2022-10-29 11:12:20 -0400

    improved entrypoint

--- services/server/docker-entrypoint.sh
+++ services/server/docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-python3 /usr/local/src/app/app.py
+# derived from cmd in docker-compose
+
+
+if [ $WEBSOCKET_ENV = "dev" ]
+then
+    # TODO: make this work
+    watchmedo auto-restart --pattern "/usr/local/src/app/*.py" --recursive --signal SIGTERM python3 /usr/local/src/app/app.py
+elif [ $WEBSOCKET_ENV = "prod" ]
+then
+    python3 /usr/local/src/app/app.py
+fi
+    
