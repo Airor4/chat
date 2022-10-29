ae70e37b58cf2fc9eb2cc82bac8f19cbdd950a90
= <=>
2022-10-29 11:13:37 -0400

    use logging instead of print because of buffer :p

--- services/server/app.py
+++ services/server/app.py
@@ -1,17 +1,20 @@
 import websockets
 import asyncio
+import logging
 
 async def handle_message(websocket):
     """Recieves a message and broadcasts it to the rest of the clients
     params
     """
-    print('blahhhh')
+    logging.info("Listening for messages...")
     async for message in websocket:
-        print(message)
+        logging.debug("message received: %s", message)
 
 async def main():
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG)
     async with websockets.serve(handle_message, "", 8763):
         await asyncio.Future()  # run forever
 
 if __name__ == '__main__':
     asyncio.run(main())
+
