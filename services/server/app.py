c369e43b784008488d79265c0c23c8b6fbd5f788
= <=>
2022-10-29 12:38:42 -0400

    use better types and respond

--- services/server/app.py
+++ services/server/app.py
@@ -1,18 +1,29 @@
-import websockets
 import asyncio
 import logging
+from websockets import server
+from websockets.server import WebSocketServerProtocol
+import secrets
 
-async def handle_message(websocket):
+
+CONNECTIONS = {}
+
+async def handle_message(websocket: WebSocketServerProtocol) -> None:
     """Recieves a message and broadcasts it to the rest of the clients
     params
     """
-    logging.info("Listening for messages...")
-    async for message in websocket:
-        logging.debug("message received: %s", message)
+    join_key = secrets.token_urlsafe(12)
+    CONNECTIONS[join_key] = websocket
+    logging.info("New player joined!")
+    try:
+        async for message in websocket:
+            logging.debug("message received: %s", message)
+            await websocket.send('fuck you')
+    finally:
+        del CONNECTIONS[join_key]
 
-async def main():
+async def main() -> None:
     logging.basicConfig(format="%(message)s", level=logging.DEBUG)
-    async with websockets.serve(handle_message, "", 8763):
+    async with server.serve(handle_message, "", 8763):
         await asyncio.Future()  # run forever
 
 if __name__ == '__main__':
